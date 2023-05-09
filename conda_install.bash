3408ef4221a87b24bef8297c6725c4e86b66d172
root <bahkhun@gmail.com>
2023-05-09 06:00:53 +0000

    Refine: ipython notebook

---
+++ conda_install.bash
@@ -0,0 +1,8 @@
+# 1
+conda install sra-tools seqkit -y
+# 2
+conda install trimmomatic -y
+# 3
+conda install bwa samtools -y
+# 4
+conda install bcftools -y
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import json
-# import tomllib
 import toml
 import time
 import sys
@@ -22,30 +21,6 @@ def time_check(func):
         end = time.time()
         return (res, end - start)
     return wrapper
-
-
-# Decorator for logging
-def with_logging(log_file):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            org_stdout = sys.stdout
-            org_stderr = sys.stderr
-            # redirect stdout & err to log file
-            with open(log_file, 'w') as f:
-                sys.stdout = f
-                sys.stderr = f
-                try:
-                    result = func(*args, **kwargs)
-                except Exception as e:
-                    print(e)
-                finally:
-                    sys.stdout = org_stdout
-                    sys.stderr = org_stderr
-
-            return result
-        return wrapper
-    return decorator
-
 
 # Multiprocessing
 def subprocs(cmd):
@@ -72,16 +47,3 @@ def execute_function_pool_args(function, args_list, pool_num):
     with Pool(pool_num) as p:
         output = p.starmap(function, args_list)
     return output
-
-# Get stat of fasta file
-def get_fasta_stat(fasta):
-    cmd = f"seqkit stat {fasta}"
-    output = subprocs(cmd)
-    data = [line.split() for line in output.stdout.decode().strip().split('\n')]
-    if len(data[1]) == 6:
-        v = [data[1][0], data[1][1], data[1][2], '0', '0', '0', '0', '0']
-        data[1] = v
-    stat_ser = pd.Series(data[1], index=data[0])
-    for col in ['num_seqs', 'sum_len', 'min_len', 'avg_len', 'max_len']:
-        stat_ser[col] = stat_ser[col].replace(',','')
-    return stat_ser
